5c790c1edae4d6974f4941691abf32304ab739ca
Dev Tomar <tomardev000007@gmail.com>
2026-01-13 18:26:29 +0530

    New Chnages

--- models/listing.js
+++ models/listing.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const Review = require("./reviews.js");
 
 const listingSchema = new Schema({
     title: {
@@ -22,12 +23,26 @@ const listingSchema = new Schema({
   url: {
     type: String,
     default: "https://images.unsplash.com/photo-1552733407-5d5c46c3bb3b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fHRyYXZlbHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
-  },
+     set: (v) => v === ""   //for client or frontend
+        ? "https://unsplash.com/photos/traditional-pagoda-surrounded-by-lush-green-trees-876UDhEV8EY" : v,   //ternary operator
+    }, 
 },
 
     price: Number,
     location: String,
     country: String,
+    reviews: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Review",
+        },
+    ],
+});
+
+listingSchema.post("findOneAndDelete", async (listing) =>{
+    if (listing) {
+    await Review.deleteMany({_id : {$in: listing.reviews}});
+    }
 });
 
 const Listing = mongoose.model("Listing", listingSchema);
